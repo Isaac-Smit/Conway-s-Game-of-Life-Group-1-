bad9fb3e2adca10b564300f7b9fa46b8a701a17a
William Busbridge <william-busbridge@live.nmit.ac.nz>
2024-04-05 08:24:04 +1300

    answers and error checking

--- forfun.js
+++ forfun.js
@@ -1,4 +1,9 @@
 //SECTION - Got user inputs working :)
 const readlineSync = require('readline-sync');
-let userAnswer = readlineSync.question('Your answer (number): ');
-console.log(userAnswer())
+let userAnswer = readlineSync.question('Please pick a number from 3-6 here: ');
+if (userAnswer === 3 || userAnswer === 4 || userAnswer === 5 || userAnswer === 6) {
+    console.log('Ah yes, what a great number that is!');
+}  else {
+    let userAnswer2 = readlineSync.question('That number is out of my range of expertise. Please pick another number from 3-6 here: ');
+    console.log('Thank you very much, your input is much appretiated!')
+}
