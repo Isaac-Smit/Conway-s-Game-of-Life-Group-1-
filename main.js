2dfd0b4efffebef3502e0e8bede53e47a3adf8c4
William Busbridge <william-busbridge@live.nmit.ac.nz>
2024-04-08 09:17:08 +1200

    readline sync

--- main.js
+++ main.js
@@ -14,7 +14,9 @@ let grid =  [[[0], [0], [0], [0], [0], [0], [0], [0], [0], [0]],
 grid.forEach(row => {
     let rowString = '';
     row.forEach(element => {
-      rowString += JSON.stringify(element);
+        rowString += JSON.stringify(element);
     });
     console.log(rowString);
-  });
+});
+
+let userAnswer = readlineSync.question('Please pick where in the grid you want to start: ');
