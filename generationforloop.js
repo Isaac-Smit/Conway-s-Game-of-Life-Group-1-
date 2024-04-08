fe043583474b734bec295860d4367c0597db8d1e
isaac-smit <isaac-smith@live.nmit.ac.nz>
2024-04-08 18:33:40 +1200

    back end of grid checking almost done

--- generationforloop.js
+++ generationforloop.js
@@ -1,7 +1,21 @@
-function checkforneighbour (){
-    for (let i = 0; i < grid.length; i++) {
-        for (let j = 0; j < grid[i].length; j++) {
 
+
+function checkCells (){
+    for (let y = 0; y < grid.length; y++) {
+        for (let x = 0; x < grid[y].length; x++) {
+            const neighbours = checkNeighbours (grid, x, y);
+            if (grid[x][y] === 1) {
+                if (neighbours < 2 || neighbours > 3) {
+                    grid[x][y] = 0;
+                }
+            } else {
+                if (neighbours === 3) {
+                    grid[x][y] = 1;
+                }
+            }
+            }
         }
     }
+function checkNeighbours (){
+    for()
 }
