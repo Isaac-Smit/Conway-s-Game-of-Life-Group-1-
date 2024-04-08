62b4ea8622cbd0c51162823b3fafcc926222e989
isaac-smit <isaac-smith@live.nmit.ac.nz>
2024-04-08 20:11:23 +1200

    usinge erins hand written notes added in code for checking neighbouring cells

--- generationforloop.js
+++ generationforloop.js
@@ -1,5 +1,5 @@
 
-
+// iterating through x and y axis of the grid
 function checkCells (){
     for (let y = 0; y < grid.length; y++) {
         for (let x = 0; x < grid[y].length; x++) {
@@ -16,6 +16,21 @@ function checkCells (){
             }
         }
     }
-function checkNeighbours (){
-    for()
+
+// checking the neighbours of cells with offset values
+function checkNeighbours (x, y){
+    let result = 0
+    for(let offsetX = -1; offsetX<=1; offsetX++) {
+        for(let offsetY = -1; offsetY<=1; offsetY++) {
+            if(!(offsetX == 0 && offsetY == 0)) {
+                const resultX = (x + offsetX + x) % x;
+                const resultY = (y + offsetY + y) % y;
+                console.log(`X = (${x} + ${offsetX} + ${x}) % ${x} = ${resultX}`);
+                console.log(`Y = (${y} + ${offsetY} + ${y}) % ${y} = ${resultY}`);
+
+                result += grid[resultX][resultY]
+            }
+        } 
+    }
+    return result;
 }
